8217e42ffb5a74b5b669ef9366c27136d9e1cde9
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-08 16:28:21 -0300

    Create eejercicio_repaso_segundo_5.c
    Xd

---
+++ Repaso_2/eejercicio_repaso_segundo_5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+int nota = atoi(argv[1]);
+if(nota >= 6) { printf("aprobado\n"); } else {
+printf("desaprobado\n"); }
+if(nota == 1) printf("Esforzarte mas\n");
+if(nota >= 8) printf("felicitaciones\n");
+}
