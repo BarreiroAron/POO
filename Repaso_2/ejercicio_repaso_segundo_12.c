097498bd91e392079374f0881cd0319b0feb65a0
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-09 15:41:14 -0300

    Create ejercicio_repaso_segundo_12.c
    xd

---
+++ Repaso_2/ejercicio_repaso_segundo_12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	float costo = 0;
+	puts("Costo de la compra");
+	scanf("%f", &costo);
+	puts("Ingrese el descuento (Numeros del uno al cien)");
+	float descuento = 0;
+	scanf("%f", &descuento);
+	float lol = costo * (descuento / 100);
+	printf("%2.f", lol);
+	return 0;
+}
