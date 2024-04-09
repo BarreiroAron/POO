ff80627f8ce86ee6504787419df22898bc45eaed
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-09 18:59:34 -0300

    Create ejercicio_repaso_segundo_14.c
    xd

---
+++ Repaso_2/ejercicio_repaso_segundo_14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    scanf("%d", &numero);
+    if(numero == 0) { printf("No te hagas el vivo"); return 0; }
+    if(numero < 0)  numero = numero * -1;
+    printf("%d", numero);
+    return 0;
+//multiplico el numero por -1 cuando es menor a 0, esto para que siempre se vuelva positivo
+}
