74f0ad1901bb0499d2fdd0081f98693ec89b6046
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-09 19:09:09 -0300

    Update ejercicio_repaso_segundo_15.c
    xd

--- Repaso_2/ejercicio_repaso_segundo_15.c
+++ Repaso_2/ejercicio_repaso_segundo_15.c
@@ -5,7 +5,9 @@ int main() {
     puts("Ingrese la cantidad de hombres y mujeres en la misma línea con un espacio de separación, por favor");
     scanf("%d %d", &hombres, &mujeres);
 
+    //sumamos el total de hombres y mujeres para saber cuanta gentuza hay en el aula
     int cantidad_total = hombres + mujeres;
+    //hacemos el promedio, multiplicandolo por cien y despues dividiendolo por la cantidad total
     float porcentaje_mujeres = (mujeres * 100.0) / cantidad_total;
     float porcentaje_hombres = (hombres * 100.0) / cantidad_total;
 
