c22b60fa5d6d87b97f78b00e65428b349cb40d82
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-04-09 18:58:18 -0300

    Update ejercicio_repaso_segundo_13.c

--- Repaso_2/ejercicio_repaso_segundo_13.c
+++ Repaso_2/ejercicio_repaso_segundo_13.c
@@ -8,6 +8,6 @@ puts("Ingrese el descuento (Numeros del uno al cien)");
 float descuento = 0;
 scanf("%f", &descuento);
 float lol = costo * (descuento / 100);
-printf("%2.f", lol);
+printf("%2.f\n", lol);
 return 0;
 }
