a7051d306d846aa0e22e8905cbcbcc9a6099824a
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-05-06 17:04:14 -0300

    Update ppt.c

--- Ejercicios_sueltos/ppt.c
+++ Ejercicios_sueltos/ppt.c
@@ -3,27 +3,27 @@
 #include <time.h>
 
 int main() {
-    char *ppt[] = {"piedra", "papel", "tijera"};
-    int puntosU = 0, puntosPC = 0;
-    int contadorUsuario = 0, contadorPC = 0;
-    int usuarioPPT, pcPPT;
+    char *ppt[] = {"piedra", "papel", "tijera"}; //Creo un array para las 3 variables
+    int puntosU = 0, puntosPC = 0; //Creo 2 variables que van a ir contando los puntos en los juegos
+    int contadorUsuario = 0, contadorPC = 0; //Creo otras 2 variables que van a ir contando los puntos en las rondas
+    int usuarioPPT, pcPPT; //Creo otras 2 variables que van a ir almacenando el numero elegido por el usuario y la computdora para competir
     
-    srand(time(NULL));
+    srand(time(NULL)); //Inicia el rand
     puts("Juguemos piedra papel o tijera!!");
     puts("No hagas tonterias o me llevo un punto. . .");
     puts("5 de 3, el que gana saca y el que pierde elige arco\n");
     
-    while(contadorUsuario < 3 || contadorPC < 3) {
-        scanf("%d", &usuarioPPT);
-        pcPPT = rand() % 3 + 1;
+    while(contadorUsuario < 3 || contadorPC < 3) { //Creo un loop para las rondas que son menores a 3 van a cumplir las siguientes condiciones
+        scanf("%d", &usuarioPPT);  //Agrego un scanf que va a ser el valor que ingrese el usuario para competir
+        pcPPT = rand() % 3; //La variable pcPPT la asgino como un numero al azar que sea hasta 3
         printf("Vos: %d   VS   Pc: %d\n", usuarioPPT, pcPPT); 
         
-        if(usuarioPPT == pcPPT) {
+        if(usuarioPPT == pcPPT) { //Creo un condicional en caso de empate
             puts("Empate! Esta jugada se repite \n");
-        } else if((usuarioPPT == 0 && pcPPT == 2) || (usuarioPPT == 1 && pcPPT == 0) || (usuarioPPT == 2 && pcPPT == 1)) {
+        } else if((usuarioPPT == 0 && pcPPT == 2) || (usuarioPPT == 1 && pcPPT == 0) || (usuarioPPT == 2 && pcPPT == 1)) { //Creo otro condicional en caso de que el usuario gane un juego
             printf("Ganas un punto\n\n");
             puntosU++;
-        } else {
+        } else { //Creo otro condicional en caso de que no ocurra ningun condicional dado y la maquina gane un juego
             printf("La pc gana un punto\n\n");
             puntosPC++;
         }
@@ -31,17 +31,17 @@ int main() {
         printf("Juegos: Tu ganaste: %d, la pc gano %d por el momento.\n", puntosU, puntosPC);
         printf("Rondas: Tu ganaste: %d, la pc gano %d por el momento.\n\n", contadorUsuario, contadorPC);
         
-        if(puntosU == 3) {
+        if(puntosU == 2) { //Creo un condicional en caso de que el usuario gane una ronda
             contadorUsuario ++;
             puntosU = 0;
             puntosPC = 0;
-        } else if(puntosPC == 3) {
+        } else if(puntosPC == 2) { //Creo otro condicional en caso de que la computadora gane una ronda
             contadorPC ++;
             puntosPC = 0;
             puntosU = 0;
         }
-        if(contadorUsuario == 3) { printf("Sacas. . . \n\n"); break; }
-        if(contadorPC== 3) { printf("Perdiste\n\n"); break; }
+        if(contadorUsuario == 3) { printf("Ganaste!! \n\n"); break; } //Creo un penultimo condicional en caso de que el usuario gane las rondas totales y por ende el juego
+        if(contadorPC== 3) { printf("Perdiste\n\n"); break; } //Creo una ultima condicion en caso de que la maquina gane las rondas totales y por ende el juego
     }
     
     return 0;
