6ac24cfbf085395f9f467960db6a5d09b8bc0fea
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-05-14 15:35:38 -0300

    Update piratas.c
    xd

--- Ejercicios_sueltos/piratas.c
+++ Ejercicios_sueltos/piratas.c
@@ -3,94 +3,174 @@
 #include <time.h>
 
 int main() {
-    int i;
+  //Creo una variable entera para las filas de la matriz
+    int i; 
+    //Creo una variable entera para el numero que ponga el usuario para el tamaño de la matriz
     int l;
-    int j;
-    int n;
-    int s;
-    int e;
-    int o;
+    //Creo una variable entera para las columnas de la matriz 
+      int j;
+    //Creo una chariable que al presionar n el pirata se desplaze hacia el norte
+    char n;
+    //Creo una chariable que al presionar s el pirata se desplaze hacia el sur
+    char s;
+      //Creo una chariable que al presionar e el pirata se desplaze hacia el este
+    char e;
+    //Creo una chariable que al presionar o el pirata se desplaze hacia el oeste
+    char o;
+     //Creo una variable donde se van a almacenar los movimientos segun la direccion
     int movimiento;
+     //Creo una variable de entero para la columna donde esta el pirata
     int pc;
+     //Creo una variable de entero para la fila donde esta el pirata
     int pf;
-    int tc;
-    int tf;
+    //Creo una variable de entero para la columna donde esta el tesoro
+    //Creo una variable de entero para la fila donde esta el tesoro
+    int tf; 
+    //Creo una variable entera para los intentos posibles de los movimientos del pirata
     int intentos = 0;
+
     puts("Ingrese el espacio de su tablero: ");
+     //El usuario ingresa un valor para el temeño de la matriz donde se va a jugar
     scanf("%d", &l);
     
-
+    //Creo 2 enteros, fila y columna que son iguales al valor que ingrese el usuario
     int fila = l, columna = l;
-    //viendo si las dimensiones son positivas
+     //Creo un if que avise que si el tamaño es menor o igual a 0 nos anule el codigo y de ese mensaje
     if (fila <= 0 || columna <= 0) {
-        printf("Las dimensiones deben ser positivas.\n");
+        printf("Las dimensiones deben ser siempre numeros y positivas.\nEl juego se termina por querer romperlo\nPdt:esto es para eduardo");
         return 1;
     }
-
+    //Si los valores son positios inicia el codigo
+    
+    
+    //Creo un array para crear la matriz
     char a[fila][columna];
+    //Activo el modo aleatorio
     srand(time(NULL));
+        //Creo un numero aleatorio para la posicion de la columna del pirata 
      pc=(rand()%(l - 2))+1;
+          //Creo un numero aleatorio para la posicion de la fila del pirata
      pf=(rand()%(l - 2))+1;
+     //Creo un numero aleatorio para la posicion de la columna del tesoro
      tc=(rand()%(l - 2))+1;
+       //Creo un numero aleatorio para la posicion de la fila del tesoro
      tf=(rand()%(l - 2))+1;
-    // int randomFila = (rand()% 6 ) +1;
-    // int randomColumna = (rand() % 6)+ 1;
+     
     // iniciar la a e imprimirla
+
+       //Creo un for para almacenar los valores de la fila
     for (int i = 0; i < fila; i++) {
+        //Creo un for para almacenar los valores de la columna
         for (int j = 0; j < columna; j++) {
-            // asignar "o" a los bordes de la a
+            //Creo un if para asignar los bordes de la matriz como 'o'
             if (i == 0 || i == fila - 1 || j == 0 || j == columna - 1) {
                 a[i][j] = 'o';
-            } else{
-
+            } else{ 
+                  /* Agrego un else que asigna 'x' el interior de la matriz 
+            y dentro de ese else agrego 2 valores, uno que es un espacio aleatorio donde se encuentre el pirata como 'P',
+            y el otro para un espacio aleatorio donde se encuentre el tesoro como 'T' */
                 a[i][j] = 'x';  // asignar 'o' al interior de la a
                 if(pf != tf && pc != tc){
                 a[pf][pc] = 'P';
-                a[tf][tc] = 'T';
+                a[tf][tc] = 'x';
                 }
             }
+             //Agrego un printf que nos va a imprimir los 'O' en los bordes y  las 'X', 'P' y 'T' en el interior de la matriz
             printf("%c ", a[i][j]);
         }
         printf("\n");
     }
 
+    if(a[pf][pc] == a[tf][tc]){
+                    puts("Encontraste el tesoro sin moverte. ¡Que suerte que tenes!");
+                    return 0;
+                }
+
+ //Creo un while que nos va a hacer funcionar el juego, que tiene un limite de 50 movimientos
     while(intentos <= 50){
-        printf("a donde te quieres mover:1 es de Norte, 2 es de Sur, 3 es de Este y 4 es de Oeste: ");
+        printf("a donde te quieres mover:\n 1 es de Norte, 2 es de Sur, 3 es de Este y 4 es de Oeste:\n");
+         //En este scanf el usuario va a moverse al norte, sur, este u oeste mediante la tecla que seleccione
         scanf("%d", &movimiento);
         printf("\n");
+                /*Creo 2 variables enteras donde van a desaparecer los antiguos espacios donde se encontraba el pirata y se 
+        actualiza segun el movimiento del usuario*/
         int pfn = pf;
         int pcn = pc;
-
+        //Creamos los movimientos los cuales se usan con: 1, 2 ,3 ,4
         if(movimiento == 1){
             a[pf - 1][pc] = 'P';
             a[pfn][pcn] = 'x';
             pf = pfn - 1;
             pc = pcn;
+            if(a[pf][pc] == a[tf][tc]){
+                    puts("Encontraste el tesoro");
+                    return 0;
+            }
         }
         else if(movimiento == 2){
             a[pf + 1][pc] = 'P';
             a[pfn][pcn] = 'x';
             pf = pfn + 1;
             pc = pcn;
+            if(a[pf][pc] == a[tf][tc]){
+                    puts("Encontraste el tesoro");
+                    return 0;
+            }
         }
         else if(movimiento == 3){
             a[pf ][pc - 1] = 'P';
             a[pfn][pcn] = 'x';
             pf = pfn;
             pc = pcn - 1;
+            if(a[pf][pc] == a[tf][tc]){
+                    puts("Encontraste el tesoro");
+                    return 0;
+            }
         }
         else if(movimiento == 4){
-            a[pf][pc] = 'P';
-            a[pfn][pcn + 1] = 'x';
+            a[pf][pc + 1] = 'P';
+            a[pfn][pcn] = 'x';
             pf = pfn;
             pc = pcn + 1;
-        }
-         
+            if(a[pf][pc] == a[tf][tc]){
+                    puts("Encontraste el tesoro");
+                    return 0;
+            }
+        } 
+        else if(movimiento <= 0) {
+        printf("El movimiento debe ser siempre el indicado anteriormente.\nEl juego se termina por querer romperl, juega normal");
+        return 0;
+    }
+    else if(movimiento >= 5){
+        printf("el numero debe ser el indicado");
+    }
+
+            /*
+        Creo 2 for que van a imprimir la matriz actualizada segun el movimiento del pirata, y hasta que cumpla su objetivo
+        */ 
              for (int i = 0; i < fila; i++) {
         for (int j = 0; j < columna; j++) {
             printf("%c ", a[i][j]);
             }
             printf("\n");
+
+            if(pf == l - 1){
+                puts("agua");
+                return 0;
+            }
+        if(pf == 0){
+                puts("agua");
+                return 0;
+            }
+        if(pc == l - 1){
+                puts("agua");
+                return 0;
+            }
+        if(pc == 0){
+                puts("agua");
+                return 0;
+            }
+
         }
         intentos++;
     }
