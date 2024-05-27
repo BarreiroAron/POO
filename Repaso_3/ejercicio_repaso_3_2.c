177c30487f1a83bbf65e37b26cca702f23577560
BarreiroAron <130598856+BarreiroAron@users.noreply.github.com>
2024-05-27 17:09:28 -0300

    Update ejercicio_repaso_3_2.c
    xd

--- Repaso_3/ejercicio_repaso_3_2.c
+++ Repaso_3/ejercicio_repaso_3_2.c
@@ -4,12 +4,12 @@
 int main() {
     int numeros[5];
 
-    printf("Introduce 5 números:\n");
+    printf("Introduce 5 numeros:\n");
     for (int i = 0; i < 5; i++) {
         scanf("%d", &numeros[i]);
     }
 
-    printf("Los números en orden inverso son:\n");
+    printf("Los numeros en orden inverso son:\n");
     for (int i = 4; i >= 0; i--) {
         printf("%d\n", numeros[i]);
     }
